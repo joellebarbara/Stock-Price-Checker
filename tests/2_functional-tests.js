600407b400220358e73ac35ce6b954daf21d5a96
Joelle Barbara <132681203+joellebarbara@users.noreply.github.com>
2024-02-11 15:44:12 +0200

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,100 +6,56 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function () {
-  let likes;
-  let rel_likes;
 
-  suite('GET /api/stock-prices => stockData object', function () {
-    test('1 stock', function (done) {
-      chai
-        .request(server)
-        .get('/api/stock-prices')
-        .query({ stock: 'goog' })
-        .end(function (err, res) {
-          assert.equal(res.status, 200);
-          assert.property(res.body.stockData, 'stock');
-          assert.property(res.body.stockData, 'price');
-          assert.property(res.body.stockData, 'likes');
-          assert.equal(res.body.stockData.stock, 'GOOG');
-          done();
-        });
-    });
+  (getUserInput) => {
+  assert(
+    !/.*\/stock-price-checker\.freecodecamp\.rocks/.test(getUserInput('url'))
+  );
+};
 
-    test('1 stock with like', function (done) {
-      chai
-        .request(server)
-        .get('/api/stock-prices')
-        .query({ stock: 'goog', like: true })
-        .end(function (err, res) {
-          assert.equal(res.status, 200);
-          assert.property(res.body.stockData, 'stock');
-          assert.property(res.body.stockData, 'price');
-          assert.property(res.body.stockData, 'likes');
-          assert.equal(res.body.stockData.stock, 'GOOG');
-          assert.isAbove(res.body.stockData.likes, 0);
-          likes = res.body.stockData.likes;
-          done();
-        });
-    });
+  
+  async (getUserInput) => {
+  const data = await fetch(getUserInput('url') + '/_api/app-info');
+  const parsed = await data.json();
+  assert.isTrue(
+    parsed.headers['content-security-policy'].includes("script-src 'self'")
+  );
+  assert.isTrue(
+    parsed.headers['content-security-policy'].includes("style-src 'self'")
+  );
+};
 
-    test('1 stock with like again (ensure likes aren\'t double counted)', function (done) {
-      chai
-        .request(server)
-        .get('/api/stock-prices')
-        .query({ stock: 'goog', like: true })
-        .end(function (err, res) {
-          assert.equal(res.status, 200);
-          assert.property(res.body.stockData, 'stock');
-          assert.property(res.body.stockData, 'price');
-          assert.property(res.body.stockData, 'likes');
-          assert.equal(res.body.stockData.stock, 'GOOG');
-          assert.equal(res.body.stockData.likes, likes);
-          done();
-        });
-    });
+  async (getUserInput) => {
+  const data = await fetch(
+    getUserInput('url') + '/api/stock-prices?stock=GOOG'
+  );
+  const parsed = await data.json();
+  assert.property(parsed, 'stockData');
+};
 
-    test('2 stocks', function (done) {
-      chai
-        .request(server)
-        .get('/api/stock-prices')
-        .query({ stock: ['goog', 'msft'] })
-        .end(function (err, res) {
-          assert.equal(res.status, 200);
-          assert.isArray(res.body.stockData);
-          assert.property(res.body.stockData[0], 'stock');
-          assert.property(res.body.stockData[0], 'price');
-          assert.property(res.body.stockData[0], 'rel_likes');
-          assert.property(res.body.stockData[1], 'stock');
-          assert.property(res.body.stockData[1], 'price');
-          assert.property(res.body.stockData[1], 'rel_likes');
-          assert.equal(res.body.stockData[0].stock, 'GOOG');
-          assert.equal(res.body.stockData[1].stock, 'MSFT');
-          assert.equal(res.body.stockData[0].rel_likes + res.body.stockData[1].rel_likes, 0);
-          rel_likes = Math.abs(res.body.stockData[0].rel_likes);
-          done();
-        });
-    });
 
-    test('2 stocks with like', function (done) {
-      chai
-        .request(server)
-        .get('/api/stock-prices')
-        .query({ stock: ['goog', 'msft'], like: true })
-        .end(function (err, res) {
-          assert.equal(res.status, 200);
-          assert.isArray(res.body.stockData);
-          assert.property(res.body.stockData[0], 'stock');
-          assert.property(res.body.stockData[0], 'price');
-          assert.property(res.body.stockData[0], 'rel_likes');
-          assert.property(res.body.stockData[1], 'stock');
-          assert.property(res.body.stockData[1], 'price');
-          assert.property(res.body.stockData[1], 'rel_likes');
-          assert.equal(res.body.stockData[0].stock, 'GOOG');
-          assert.equal(res.body.stockData[1].stock, 'MSFT');
-          assert.equal(res.body.stockData[0].rel_likes + res.body.stockData[1].rel_likes, 0);
-          assert.equal(Math.abs(res.body.stockData[0].rel_likes), rel_likes);
-          done();
-        });
-    });
-  });
+  async (getUserInput) => {
+  const data = await fetch(
+    getUserInput('url') + '/api/stock-prices?stock=GOOG'
+  );
+  const parsed = await data.json();
+  const ticker = parsed.stockData;
+  assert.typeOf(ticker.price, 'number');
+  assert.typeOf(ticker.likes, 'number');
+  assert.typeOf(ticker.stock, 'string');
+};
+
+
+  async (getUserInput) => {
+  const data = await fetch(
+    getUserInput('url') + '/api/stock-prices?stock=GOOG&stock=MSFT'
+  );
+  const parsed = await data.json();
+  const ticker = parsed.stockData;
+  assert.typeOf(ticker, 'array');
+  assert.property(ticker[0], 'rel_likes');
+  assert.property(ticker[1], 'rel_likes');
+};
+  
+
 });
